9c32b95b5ad124dee5dd7a70eff0e7c1ba049670
Nishant Singh <ns777777711@gmail.com>
2022-06-19 13:10:01 +0530

    added

--- projectnish/optional/optional/projectnishant/navbar/index.js
+++ projectnish/optional/optional/projectnishant/navbar/index.js
@@ -1,3 +1,12 @@
+// var isfalse=true
+let docheck=JSON.parse(localStorage.getItem("login"))
+console.log(docheck)
+if(docheck===null){
+  document.querySelectorAll(".upd")[0].innerText="login"
+  console.log(3);
+  isfalse=true
+}
+
 let ls=localStorage.getItem("pin");
 document.querySelector("#enterpin").value=ls || "Enter Pin"
 document.querySelector("#span2").addEventListener("click",function(){
@@ -25,70 +34,96 @@ document.querySelector("#span2").addEventListener("click",function(){
 //   let x=document.querySelector("#search").setAttribute("placeholder","Start shopping...")
 
   document.querySelector("#enterpindiv").addEventListener("click",function(){
-    let x=document.querySelector(".popup_pin")
+    let x=document.querySelector(".ppopup_pin")
     x.style.display="flex"
   })
 
-  document.querySelector(".close").addEventListener("click",function(){
-   let x= document.querySelector(".popup_pin");
+  document.querySelector(".closee").addEventListener("click",function(){
+   let x= document.querySelector(".ppopup_pin");
    x.style.display="none"
   })
   document.querySelector("#btnAddPin").addEventListener("click",function(){
-   let y= document.querySelector("#txt").value;
+   let y= document.querySelector("#txtt").value;
    localStorage.setItem("pin",y);
-//    let x= document.querySelector(".popup_pin");
-//    x.style.display="none"
+   let x= document.querySelector(".ppopup_pin");
+   x.style.display="none"
       window.location.reload()
-//   if(document.querySelector("#txt").value!=""){
-//     document.querySelector("#enterpin").value=document.querySelector("#txt").value;
+  if(document.querySelector("#txtt").value!=""){
+    document.querySelector("#enterpin").value=document.querySelector("#txtt").value;
   
-//   }
+  }
 })
 document.querySelector("#nexttopin").addEventListener("change",function(){
     localStorage.removeItem("pin");
     
    })
-   var mensData = [
-    {
-      image_url:
-        "https://content.shop4reebok.com/static/Product-EX4296/reebok_EX4296_1.jpg.plp",
-      name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
-      price: "1170",
-      strikedoffprice: "$2599",
-      quantity:1,
-    },
-    {
-      image_url:
-        "https://content.shop4reebok.com/static/Product-EX3921/reebok_EX3921_1.jpg.plp",
-      name: "MEN'S REEBOK SWIM ARUBA FLIP SLIPPERS ",
-      price: 699,
-      strikedoffprice: 999,
-      quantity:1,
-    }
-   ]
-//    res()
-let arrdata=[]
-    let res=mensData.forEach(function(el){
-        let divs=document.createElement("div");
-        let imgs=document.createElement("img");
-        imgs.src=el.image_url;
-        let texts1=document.createElement("h2");
-        texts1.innerText=el.name;
-        let texts2=document.createElement("h2");
-        texts2.innerText=el.price;
-        let btn=document.createElement("button");
-        btn.innerText="Add to cart";
-        btn.addEventListener("click",function(){
-          console.log(el)
-          arrdata.push(el)
-          console.log(arrdata)
-          localStorage.setItem("cartmei",JSON.stringify(arrdata))
-        })
-        divs.append(imgs,texts1,texts2,btn);
-         document.querySelector("#check").append(divs)
-      })
-      // document.querySelectorAll(".upd")[0].innerText="logout";
-      // document.querySelectorAll(".upd")[1].innerText="";
-      // document.querySelectorAll(".upd")[2].innerText="";
-      // document.querySelectorAll(".upd")[0].style.marginleft="70px"
-
+//    var mensData = [
+//     {
+//       image_url:
+//         "https://content.shop4reebok.com/static/Product-EX4296/reebok_EX4296_1.jpg.plp",
+//       name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+//       price: "1170",
+//       strikedoffprice: "$2599",
+//       quantity:1,
+//     },
+//     {
+//       image_url:
+//         "https://content.shop4reebok.com/static/Product-EX3921/reebok_EX3921_1.jpg.plp",
+//       name: "MEN'S REEBOK SWIM ARUBA FLIP SLIPPERS ",
+//       price: 699,
+//       strikedoffprice: 999,
+//       quantity:1,
+//     }
+//    ]
+// //    res()
+// let arrdata=[]
+//     let res=mensData.forEach(function(el){
+//         let divs=document.createElement("div");
+//         let imgs=document.createElement("img");
+//         imgs.src=el.image_url;
+//         let texts1=document.createElement("h2");
+//         texts1.innerText=el.name;
+//         let texts2=document.createElement("h2");
+//         texts2.innerText=el.price;
+//         let btn=document.createElement("button");
+//         btn.innerText="Add to cart";
+//         btn.addEventListener("click",function(){
+//           console.log(el)
+//           arrdata.push(el)
+//           console.log(arrdata)
+//           localStorage.setItem("cartmei",JSON.stringify(arrdata))
+//         })
+//         divs.append(imgs,texts1,texts2,btn);
+//          document.querySelector("#check").append(divs)
+//       })
+ 
+  // else{
+let checking=JSON.parse(localStorage.getItem("login"))
+if(checking!==null){
+  console.log(10)
+       document.querySelectorAll(".upd")[0].innerText="logout";
+       
+       document.querySelectorAll(".upd")[1].innerText="";
+       document.querySelectorAll(".upd")[2].innerText="";
+       document.querySelectorAll(".upd")[0].style.marginleft="70px"
+     var isfalse=false
+}
+if(isfalse===false){
+  document.querySelectorAll(".upd")[0].addEventListener("click",function(){
+    localStorage.removeItem("login");
+    window.location.reload()
+   })
+   }
+  // }
+  if(isfalse===true){
+document.querySelector("#loginnav").addEventListener("click",function(){
+  console.log(3)
+  location.href="login.html";
+})
+  }
+document.querySelector("#regnav").addEventListener("click",function(){
+  location.href="mreg.html";
+})
+document.querySelector("#cartnav").addEventListener("click",function(){
+  location.href="linkcart.html";
+})
---
+++ projectnish/optional/optional/projectnishant/navbar/mhome.js
@@ -0,0 +1,26 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+
+var counter=1;
+setInterval(function(){
+    document.getElementById('radio'+counter).checked=true;
+    counter++;
+    if(counter>9){
+        counter=1;
+    }
+},5000);
